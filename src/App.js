3f170f137a7f34f911071493ddb3ad260c91c97d
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-09-12 00:02:34 +0530

    search bar and logo has created

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Box } from "@mui/system";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import VideoDetail from "./components/VideoDetail";
+/* import VideoDetail from "./components/VideoDetail";
 import ChannelDetail from "./components/ChannelDetail";
 import Feed from "./components/Feed";
 import SearchFeed from "./components/SearchFeed";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar"; */
+
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 function App() {
   return (
