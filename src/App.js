536e57715938d0a9893916c592c2224fcf6276cc
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-09-11 23:29:58 +0530

    basic folder are created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+
+import { Box } from "@mui/system";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import VideoDetail from "./components/VideoDetail";
+import ChannelDetail from "./components/ChannelDetail";
+import Feed from "./components/Feed";
+import SearchFeed from "./components/SearchFeed";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Navbar} from './Navbar'
+export {default as Navbar} from './Navbar'
+export {default as Navbar} from './Navbar'
+export {default as Navbar} from './Navbar'
+export {default as Navbar} from './Navbar'
