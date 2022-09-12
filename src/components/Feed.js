515ba51602dc239770b2ca44480e00f484eebe19
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-09-12 19:57:30 +0530

    sidebar done

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,24 @@
+import { Box, Stack, Typography } from '@mui/material'
 import React from 'react'
+import Sidebar from './Sidebar'
 
 function Feed() {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <Sidebar />
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 Smukherjee
+        </Typography>
+
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
-export default Feed
+export default Feed                                                                    
--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,5 @@ export { default as ChannelDetail } from "./ChannelDetail";
 export { default as SearchFeed } from "./SearchFeed";
 export { default as VideoDetail } from "./VideoDetail";
 export { default as Searchbar } from "./Searchbar";
+export { default as Sidebar } from "./Sidebar";
 
