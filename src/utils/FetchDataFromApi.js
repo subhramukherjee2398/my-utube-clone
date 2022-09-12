6f6c31418fd738f787d67d0fe043096aaa3d8d46
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-09-12 20:52:09 +0530

    env setup

--- src/utils/FetchDataFromApi.js
+++ src/utils/FetchDataFromApi.js
@@ -7,8 +7,8 @@ const options = {
   },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
-  },
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
 };
 export const FetchDataFromApi = async(url) =>{
   const {data} = await axios.get(`${BASE_URL}/${url}`, options)
