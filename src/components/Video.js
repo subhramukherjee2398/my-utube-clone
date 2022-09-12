6dede984ef4e3559c605c7edbe0378df389f3599
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-09-12 22:53:15 +0530

    Loader implemented

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,15 +1,18 @@
 import { Box, Stack } from '@mui/material'
 import React from 'react'
+import Loader from './Loader';
 import VideoCard from './VideoCard'
 
 function Video({ videos }) {
+  if(!videos?.length) return <Loader />;
   return (
     <Stack flexWrap="wrap" justifyContent="start" alignItems="start" gap={2} flexDirection='row'>
       {videos.map((item, id) => (
-        <Box key={id}>
-          {item?.id?.videoId && <VideoCard video={item} />}
-        </Box>
-      ))}
+          <Box key={id}>
+            <Loader />
+            {item?.id?.videoId && <VideoCard video={item} />}
+          </Box>
+        ))}
     </Stack>
   )
 }
