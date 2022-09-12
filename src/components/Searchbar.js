1ec42ab1e36b29e7f2ea23ba41b125b3efce9d56
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-09-12 11:22:05 +0530

    search bar design done

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,8 +1,27 @@
+import { IconButton, Paper } from '@mui/material'
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
 
 function Searchbar() {
   return (
-    <div>Searchbar</div>
+    <Paper
+      component='form'
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 1,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className='search-bar'
+        placeholder='Search...'
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   )
 }
 
