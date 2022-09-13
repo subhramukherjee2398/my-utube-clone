ecc997bc08b0146a8be07d8efeecfcab2c7ca79e
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-09-14 00:42:26 +0530

    videoDeatils complete

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,49 +4,54 @@ import ReactPlayer from 'react-player'
 import { Link } from 'react-router-dom'
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { FetchDataFromApi } from '../utils/FetchDataFromApi';
-
+import Video from './Video';
+import { useParams } from 'react-router-dom'
 function VideoDetail() {
-  const id = useState('');
+  const { id } = useParams()
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
 
-  useEffect(()=>{
-   FetchDataFromApiforVideo();
-     
-  },[id])
+  useEffect(() => {
+    FetchDataFromApiforVideo();
+  }, [id])
 
-  const FetchDataFromApiforVideo  = async() =>{
-    FetchDataFromApi(`videos?part=snippet,statistics&id=${id}`).then((ele)=>console.log(ele))
-    FetchDataFromApi(`search?part=snippet&relatedToVideoId=${id}&type=video`).then((ele)=>console.log(ele))
- }
+  const FetchDataFromApiforVideo = async () => {
+    console.log('call', id)
+    FetchDataFromApi(`videos?part=snippet,statistics&id=${id}`).then((ele) => setVideoDetail(ele.items[0])).catch((e) => console.warn(e))
+    FetchDataFromApi(`search?part=snippet&relatedToVideoId=${id}&type=video`).then((ele) => setVideos(ele.items)).catch((e) => console.warn(e))
+  }
 
+
+  console.log(videoDetail);
   return (
     <Box minHeight="95vh">
       <Stack direction={{ xs: "column", md: "row" }}>
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
-            <ReactPlayer  className="react-player" controls />
+            <ReactPlayer className="react-player" controls />
             <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
-              {}
+              {videoDetail?.snippet?.title}
             </Typography>
             <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
-              
-                <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
-                 
-                  <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
-                </Typography>
-              
+
+              <Typography variant={{ sm: "subtitle1", md: 'h6' }} color="#fff" >
+                {videoDetail?.snippet?.channelTitle}
+                <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+              </Typography>
+
               <Stack direction="row" gap="20px" alignItems="center">
                 <Typography variant="body1" sx={{ opacity: 0.7 }}>
-                   views
+                  {parseInt(videoDetail?.snippet?.viewCount).toLocaleString()} views
                 </Typography>
                 <Typography variant="body1" sx={{ opacity: 0.7 }}>
-                  likes
+                  {parseInt(videoDetail?.snippet?.likeCount).toLocaleString()} likes
                 </Typography>
               </Stack>
             </Stack>
           </Box>
         </Box>
-        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
-        
+        <Box px={1} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems="center" >
+          <Video videos={videos} direction="column" />
         </Box>
       </Stack>
     </Box>
